e7c4c6dbd04482e13ebb734adf832e78524fb81d
Takahiro Masuda <takahiro.masuda@salesforce.com>
2020-10-19 21:36:29 +0900

    modify api controller

--- app/Customize/Controller/GetCategoriesController.php
+++ app/Customize/Controller/GetCategoriesController.php
@@ -14,12 +14,15 @@ class GetCategoriesController
      */
     public function testMethod()
     {
-        $fruits_array = ['apple'=>'fruits1',
-                 'orange'=>'fruits2',
-                 'melon'=>'fruits3',
-                 'pineapple'=>'fruits4'
-                ];
-        $jsonstr =  json_encode($fruits_array);
-        return new Response($jsonstr);
+        $fruits_array = [
+            'apple'=>'fruits1',
+            'orange'=>'fruits2',
+            'melon'=>'fruits3',
+            'pineapple'=>'fruits4'
+        ];
+        header("Content-Type: application/json; charset=utf-8");
+        echo json_encode($jsonstr);
+        //$jsonstr =  json_encode($fruits_array);
+        //return new Response($jsonstr);
     }
 }
