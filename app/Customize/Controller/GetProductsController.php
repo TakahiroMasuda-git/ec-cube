5c773c135509996240a4af7ffddbc6e8a909a3e7
Takahiro Masuda <takahiro.masuda@salesforce.com>
2020-10-21 10:19:38 +0900

    modify api controller

--- app/Customize/Controller/GetProductsController.php
+++ app/Customize/Controller/GetProductsController.php
@@ -8,15 +8,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 //https://doc4.ec-cube.net/customize_controller
-class GetCategoriesController extends AbstractController
+class GetProductsController extends AbstractController
 {
     /**
      * @Method("GET")
-     * @Route("/getCategories")
+     * @Route("/getProducts")
      */
     public function testMethod()
     {
-        $categories->categories = [
+        $products->products = [
             ["id"=>"7","name"=>"WOMAN"],
             ["id"=>"8","name"=>"MEN"],
             ["id"=>"9","name"=>"KIDS"]
